3233a1cc83b395aefad94cdb22ca433b042cb669
75c_100006496246_bastenyang <75c_100006496246_bastenyang@git.cloud.tencent.com>
2018-09-17 18:31:11 +0800

    service层的video方法增加

--- src/main/java/com/imooc/service/VideoService.java
+++ src/main/java/com/imooc/service/VideoService.java
@@ -1,10 +1,13 @@
 package com.imooc.service;
 
 import com.imooc.pojo.Videos;
+import com.imooc.utils.PagedResult;
 
 public interface VideoService {
 	
 	public String saveVideo(Videos video);
 	
 	public void updateVideo(String videoId,String coverPath);
+	
+	public PagedResult getAllVideos(Integer page,Integer pageSize);
 }
--- src/main/java/com/imooc/service/impl/VideoServiceImpl.java
+++ src/main/java/com/imooc/service/impl/VideoServiceImpl.java
@@ -1,14 +1,21 @@
 package com.imooc.service.impl;
 
+import java.util.List;
+
 import org.n3r.idworker.Sid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.imooc.mapper.VideosMapper;
+import com.imooc.mapper.VideosMapperCustom;
 import com.imooc.pojo.Videos;
+import com.imooc.pojo.vo.VideosVO;
 import com.imooc.service.VideoService;
+import com.imooc.utils.PagedResult;
 
 @Service
 public class VideoServiceImpl implements VideoService {
@@ -18,6 +25,10 @@ public class VideoServiceImpl implements VideoService {
 	
 	@Autowired
 	private VideosMapper videosMapper;
+	
+
+	@Autowired
+	private VideosMapperCustom videosMapperCustom;
 
 	@Transactional(propagation = Propagation.REQUIRED)
 	@Override
@@ -37,5 +48,19 @@ public class VideoServiceImpl implements VideoService {
 		video.setId(videoId);
 		video.setCoverPath(coverPath);
 		videosMapper.updateByPrimaryKeySelective(video);
+	}
+
+	@Override
+	public PagedResult getAllVideos(Integer page, Integer pageSize) {
+		// TODO Auto-generated method stub
+		PageHelper.startPage(page, pageSize);
+		List<VideosVO> list = videosMapperCustom.queryAllVideos();
+		PageInfo<VideosVO> pageList = new PageInfo<>(list);
+		PagedResult pagedResult = new PagedResult();
+		pagedResult.setPage(page);
+		pagedResult.setTotal(pageList.getPages());
+		pagedResult.setRows(list);
+		pagedResult.setRecords(pageList.getTotal());
+		return pagedResult;
 	}
 }
